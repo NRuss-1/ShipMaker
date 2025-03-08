67c983df121beabf3944800ef6de64bddb0a420a
NRuss-1 <84658158+NRuss-1@users.noreply.github.com>
2025-03-07 23:33:44 -0500

    Update Composition.py

--- autoShip/Composition.py
+++ autoShip/Composition.py
@@ -1,15 +1,20 @@
-import Ships
 
 class Composition:
-    def __init__(self, comp):
+    def __init__(self, comp, instanceData):
+        self.instanceData = instanceData
         self.comp = comp
+        self.cp = 0
+        self.hp = 0
+        self.mannning = 0 
+        self.dps = 0
+        self.speed = 0
 
     def getHP(self):
         hp = 0
         for boat in self.comp:
             hp += boat.HP
         return hp
-    
+
     def getNames(self):
         names = []
         for boat in self.comp:
@@ -23,6 +28,10 @@ class Composition:
             return False 
         if self.getCP() < maxCP - (maxCP*.1):
             return False
+        if self.instanceData.delimeter[0] != '':
+            for boat in self.instanceData.delimeter:
+                if boat not in self.getNames():
+                    return False
         return True
 
     def getCP(self):
@@ -49,11 +58,19 @@ class Composition:
             speed += boat.speed
         return speed
     
+    def getManningRatio(self):
+        max_people = self.instanceData.num_people
+        manning = self.getManning()
+        return manning/max_people
+
+    def getCompSize(self):
+        return len(self.comp)
+    
     def getRelativePower(self):
         hp = self.getHP()
         dps = self.getDPS()
         speed = self.getSpeed()
         cp = self.getCP()
         
-        rel_power = (hp + speed + dps) * cp * (1/len(self.comp))
+        rel_power = (hp + speed + dps) * (1.5 *cp) * (1/self.getCompSize()) * (1.5*self.getManningRatio())
         return rel_power
