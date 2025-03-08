93d80a2f03c7f22896c84ae3870b956e6c9b87c0
NRuss-1 <84658158+NRuss-1@users.noreply.github.com>
2025-03-07 23:34:15 -0500

    Update Interface.py

--- autoShip/Interface.py
+++ autoShip/Interface.py
@@ -2,24 +2,44 @@ import tkinter as tk
 import BoatMath
 import Instance
 
-instanceData = Instance.InstanceData()
+
+instanceData= Instance.InstanceData().getInstance()
 
 def makeUI():
     window = tk.Tk()
     window.title("Ship Finder")
-    window.geometry("300x150")
+    window.geometry("300x250")
 
     text_label = tk.Label(window, text = "Enter the number of people you have")
     text_label.pack()
     text_entry = tk.Entry(window, width = 10)
     text_entry.pack()
+
+    text_label2 = tk.Label(window, text = "Enter ship delimeter")
+    text_label2.pack()
+    text_entry2 = tk.Entry(window, width = 30)
+    text_entry2.pack()
+
+    pango = tk.IntVar()
+    ares = tk.IntVar()
+    hind = tk.IntVar()
+    pango_button = tk.Checkbutton(window, text = "Show Pangolins!", variable=pango, onvalue=1, offvalue=0)
+    ares_button = tk.Checkbutton(window, text = "Show Ares!", variable=ares, onvalue=1, offvalue=0)
+    hind_button = tk.Checkbutton(window, text = "Show Hinds!", variable=hind, onvalue=1, offvalue=0)
+    pango_button.pack()
+    ares_button.pack()
+    hind_button.pack()
+
     def get_input():
         text = text_entry.get()
+        text2 = text_entry2.get()
         instanceData.initCombArr()
         instanceData.setNumPeople(int(text))
         instanceData.initPage()
-        BoatMath.createCompositions(int(text), instanceData)
-        lst = BoatMath.formatComps(instanceData)
+        instanceData.setShips(pango.get(),ares.get(),hind.get())
+        instanceData.setDelim(text2)
+        BoatMath.createCompositions()
+        lst = BoatMath.formatComps()
         displayValues(lst)
 
     submit_button = tk.Button(window, text = "Submit", command = get_input)
@@ -47,12 +67,12 @@ def displayValues(lst):
 
     def hit_next():
         instanceData.getNextPage()
-        lst = BoatMath.formatComps(instanceData)
+        lst = BoatMath.formatComps()
         displayValues(lst)
     
     def hit_previous():
         instanceData.getPreviousPage()
-        lst = BoatMath.formatComps(instanceData)
+        lst = BoatMath.formatComps()
         displayValues(lst)
 
     table = Table(window, lst)
